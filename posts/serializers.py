e6d422c3d833e27058e37a25a04649f592905223
kostya1102m <122353133+kostya1102m@users.noreply.github.com>
2026-03-23 21:38:01 +0900

    refactor:

--- posts/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        fields = ('id', 'text', 'author', 'image', 'pub_date')
-        read_only_fields = ('author', )
-        model = Post
